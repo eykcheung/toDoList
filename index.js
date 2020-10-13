d60fb55c9abf0acdd12d11c6bf6a96cc92cb1b3b
eykcheung <eykcheung@gmail.com>
2020-10-13 12:54:00 +0800

    Mark A Task as Complete

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ $(document).ready(function () {
         $('#todo-list').empty();    //this code clears out all the existing tasks in the HTML before injecting the latest tasks
         response.tasks.forEach(function (task) {
           //console.log(task.content);
-          $('#todo-list').append('<div class = "row"><p class = "col-xs-8">' + task.content + '</p><button class = "delete" data-id="' + task.id + '">Delete</button>');
-        })
+          $('#todo-list').append('<div class = "row"><p class = "col-xs-8">' + task.content + '</p><button class = "delete" data-id="' + task.id + '">Delete</button><input type = "checkbox" class = "mark-complete" data-id = "' + task.id + '"' + (task.completed? 'checked' : '') + '>');
+        });
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -67,3 +67,23 @@ $(document).ready(function () {
   })
 
 });
+
+var markTaskComplete = function (id) {
+  $.ajax({
+    type: 'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=199',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      getAndDisplayAllTasks;
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+$(document).on('change', '.mark-complete', function () {
+  if (this.checked) {
+    markTaskComplete($(this).data('id'));
+  }
+})
